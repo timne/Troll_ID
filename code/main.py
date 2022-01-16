193eb969647047eaebff921dcf208469d3fce283
Liam Tyler <tyler.liam7@gmail.com>
2022-01-16 16:20:01 -0600

    beginning of final display image

--- code/main.py
+++ code/main.py
@@ -5,49 +5,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 from PIL import ImageGrab
 
-def get_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-
-def remove_noise(image):
-    return cv2.medianBlur(image,5)
-
-def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
-def dilate(image):
-    kernel = np.ones((5,5),np.uint8)
-    return cv2.dilate(image, kernel, iterations = 1)
-
-def erode(image):
-    kernel = np.ones((5,5),np.uint8)
-    return cv2.erode(image, kernel, iterations = 1)
-
-#opening - erosion followed by dilation
-def opening(image):
-    kernel = np.ones((5,5),np.uint8)
-    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-
-def canny(image):
-    return cv2.Canny(image, 100, 200)
-
-#skew correction
-def deskew(image):
-    coords = np.column_stack(np.where(image > 0))
-    angle = cv2.minAreaRect(coords)[-1]
-    if angle < -45:
-        angle = -(90 + angle)
-    else:
-        angle = -angle
-    (h, w) = image.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    return rotated
-
-#template matching
-def match_template(image, template):
-    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
-
+def load_image( filename ):
+    img = cv2.imread( filename )
+    if img is None:
+        print( "Could not load image '" + filename + "'" )
+        exit( 0 )
+    img = cv2.cvtColor( img, cv2.COLOR_BGR2RGB )
+    return img
 
 def find_white(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -99,7 +63,6 @@ def invert_image( image ):
 def take_screenshot():
     snapshot = ImageGrab.grab()
     snapshot.save( "test_images/LT_fullscreen_3.png" )
-
 
 def get_names( image ):
     height = image.shape[0]
@@ -178,18 +141,25 @@ def get_cropped_images( img ):
 
     return croppedImages
 
+def create_final_display_image( croppedImgs, trolls ):
+    if len( trolls ) == 0:
+        img = load_image( "no_trolls.png" )
+        cv2.imshow('cropped', img )
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        for troll in trolls:
+            row = troll[0]
+            col = troll[1]
+
 if __name__ == "__main__":
     filename = "test_images/LT_fullscreen_1.png"
-    img = cv2.imread( filename )
-    if img is None:
-        print( "Could not load image '" + filename + "'" )
-        exit( 0 )
-    img = cv2.cvtColor( img, cv2.COLOR_BGR2RGB )
+    img = load_image( filename )
 
     croppedImgs = get_cropped_images( img )
 
     img2 = find_white( img )
-    imgGray = get_grayscale( img2 )
+    imgGray = cv2.cvtColor( img2, cv2.COLOR_RGB2GRAY)
     #cv2.imwrite( "test_images/test2_gray3.png", imgGray )
     threshVal, threshImg = cv2.threshold( imgGray, 230, 255, cv2.THRESH_BINARY )
     # cv2.imwrite( "test_images/test2_gray3.png", threshImg )
