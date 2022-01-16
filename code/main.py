a31335bbf123e1c046306733f3212801247ad920
Liam Tyler <tyler.liam7@gmail.com>
2022-01-15 21:59:03 -0600

    some initial grayscale thresholding testing

---
+++ code/main.py
@@ -0,0 +1,58 @@
+import cv2
+import pytesseract
+import numpy as np
+from matplotlib import pyplot as plt
+
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+ 
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+def dilate(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
+    
+def erode(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.erode(image, kernel, iterations = 1)
+
+#opening - erosion followed by dilation
+def opening(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+def canny(image):
+    return cv2.Canny(image, 100, 200)
+
+#skew correction
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
+
+#template matching
+def match_template(image, template):
+    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED) 
+
+if __name__ == "__main__":
+    filename = "test_images/test1.png"
+    img = cv2.imread( filename )
+    imgGray = get_grayscale( img )
+    #cv2.imwrite( "test_images/test1_gray.png", imgGray )
+    
+    ret, threshImg = cv2.threshold( imgGray, 254, 255, cv2.THRESH_BINARY )
+
+    plt.imshow( threshImg, cmap='gray' )
+    plt.show()
