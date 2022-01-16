589b5112a77b5af9220a3dc60ec145e9b82586e5
Liam Tyler <tyler.liam7@gmail.com>
2022-01-16 13:57:32 -0600

    adding 2nd function to keep only pure white pixel

--- code/main.py
+++ code/main.py
@@ -51,19 +51,45 @@ def match_template(image, template):
 def find_white(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    lower_white = np.array([0, 0, 235])
-    upper_white = np.array([179, 20, 255])
+    lower_white = np.array([0, 0, 220])
+    upper_white = np.array([179, 35, 255])
 
     mask = cv2.inRange(hsv, lower_white, upper_white)
 
     # mask = cv2.bitwise_not(mask)
     res = cv2.bitwise_and(image, image, mask= mask)
 
-    cv2.imshow('image', res)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow('image', res)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     return res
+
+# input: colored RGB image
+# output: a new image, where any pixel that touches (orthogonally or diagonally) a pure white pixel
+#         is set to be pure white and all other pixels are set to black
+def keep_only_white( image ):
+    height = image.shape[0]
+    width = image.shape[1]
+    dstImage = np.zeros( ( height, width ), np.uint8 )
+    for row in range( height ):
+        for col in range( width ):
+            # touchesPureWhite = False
+            # for dR in range( -1, 2 ):
+            #     for dC in range( -1, 2 ):
+            #         r = min( height - 1, max( 0, row + dR ) )
+            #         c = min( width - 1, max( 0, col + dC ) )
+            #         p = image[r][c]
+            #         if p[0] == 255 and p[1] == 255 and p[2] == 255:
+            #             touchesPureWhite = True
+            #             break
+            # if touchesPureWhite:
+            #     dstImage[row][col] = 255
+            p = image[row][col]
+            if p[0] == 255 and p[1] == 255 and p[2] == 255:
+                dstImage[row][col] = 255
+
+    return dstImage
 
 if __name__ == "__main__":
     filename = "test_images/test2.png"
@@ -73,14 +99,14 @@ if __name__ == "__main__":
         exit( 0 )
 
     img2 = find_white( img )
-
-    imgGray = get_grayscale( img )
-    #cv2.imwrite( "test_images/test1_gray.png", imgGray )
+    imgGray = get_grayscale( img2 )
+    #cv2.imwrite( "test_images/test2_gray3.png", imgGray )
 
     threshVal, threshImg = cv2.threshold( imgGray, 230, 255, cv2.THRESH_BINARY )
+    #threshImg = keep_only_white( img )
 
-    plt.imshow( threshImg, cmap='gray' )
-    plt.show()
+    #plt.imshow( threshImg, cmap='gray' )
+    #plt.show()
 
     # no idea what this config does, just copied from the blog
     custom_config = r'--oem 3 --psm 6'
